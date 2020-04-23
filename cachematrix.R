45947e78ab18413c924cd4bc9d2695b4163a75dd
Kevin K <63257001+BobbyJones24@users.noreply.github.com>
2020-04-23 12:47:20 -0500

    Updated code to show in GitHub

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function creates the inverse of a matrix and stores it in the cache memory
 
-## Write a short comment describing this function
+## Creates the inverse of the matrix entered into the function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        if(!is.matrix(x)) {
+                stop("Please enter a matrix as the function argument")
+        } else {
+                y <- solve(x)
+                cachedmatrix <<- y
+        }
+        if(!is.matrix(cachedmatrix)) {
+                stop("Matix is not invertible")
+        } else  {
+                print("matrix inverted and stored in cachedmatrix")
+        }        
 }
 
 
-## Write a short comment describing this function
+## Creates the inverse of matrix x, checks first to see if the inverse has already been
+## calculated and stored in memory.  Pulls first from memory if it exists, if not calculates
+## the inverse using solve()
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        if(!is.null(cachedmatrix)) {
+                y <- cachedmatrix
+        } else {
+                y <- solve(x)
+                cachedmatrix <<- y       
+        }
+        print(y)
 }
